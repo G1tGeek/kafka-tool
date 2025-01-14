49796705f4aeb114b7d4d81498b51cd20ca04928
Yuvraj Poonia <166198082+G1tGeek@users.noreply.github.com>
2025-01-14 08:19:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,5 +41,5 @@ application-host ${module.compute.application_private_ip}
 kafka-host ${module.compute.kafka_private_ip}
 database-host ${module.compute.database_private_ip}
 EOT
-  filename = "outputs.txt"
+  filename = "ansible/outputs.txt"
 }
