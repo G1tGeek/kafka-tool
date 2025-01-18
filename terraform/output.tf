38530feb3847406a2116bbdd8605aa194ed32cb1
Yuvraj Poonia <166198082+G1tGeek@users.noreply.github.com>
2025-01-18 17:20:46 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,9 @@
-output "kafka_private_ip" {
+output "kafka1_private_ip" {
   value       = module.compute.kafka1_private_ip
+}
+output "kafka2_private_ip" {
+  value       = module.compute.kafka2_private_ip
+}
+output "kafka3_private_ip" {
+  value       = module.compute.kafka3_private_ip
 }
